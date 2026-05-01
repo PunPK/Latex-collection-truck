066667725da0bd5ac1ff3372ef19ac01fcb9607d
PunPK <phurutsakorn.kps@gmail.com>
2026-05-01 15:00:51 +0700

    init website frontend

---
+++ frontend-latex/src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import Header from './Header.svelte';
+
+    let name = $state('PunPK');
+    let status: 'success' | 'error' = $state('success');
+
+    function ToggleEvent() {
+        status = status === 'success' ? 'error' : 'success';
+    }
+
+    let fullName = $derived(name + " " + 'Desr')
+
+</script>
+
+<Header name={name} se_name='wes' />
+<h2>{fullName}</h2>
+
+<input type="text" bind:value={name} />
+
+<p>Status: {status}</p>
+<button onclick={ToggleEvent}>Toggle Status</button>
---
+++ frontend-latex/src/routes/Header.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    let { name, se_name = 'wes' } : {name : string; se_name? : string;} = $props();
+</script>
+
+<h1>Header Hello {name}</h1>
+<h2>{name.replaceAll('K', '-')}</h2>
+
+{se_name}
+
+<style>
+
+
+</style>
